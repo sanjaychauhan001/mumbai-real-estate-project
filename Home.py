955ae1f6d0d314f2ab6b1c1c017086ff1a23c378
sanjay chauhan <sanjayjustforyou123@gmail.com>
2024-11-14 19:41:52 +0530

    modified

--- Home.py
+++ Home.py
@@ -7,7 +7,7 @@ st.header("This project is build on mumbai's 8500 flats dataset.")
 
 st.write("1. The data was scraped from the housing.com using selenium and BeautifulSoup.")
 st.write("2. The data cleaning was done in jupyter notebook using pandas and numpy.")
-st.write("3. Done the univariate ,bivariate and exploratory data analysis using matplotlib and seaborn to get the understanding of data.")
+st.write("3. Done the exploratory data analysis using matplotlib and seaborn to get the understanding of data.")
 st.write("4. Performed feature engineering, outlier detection and feature selection.")
 st.write("5. Detected the outliers using Box plot, histogram and IQR method. to do the feature selection utilized statistical test like anova and applied the tree algorithm to get the feature importance.")
 st.write("6. Applied different different algorithm and got best result on SVR then applied the grid searchSV to get the best parametrs.")
